c821bee6d4374ff57adc4f1c94b74f4bd3dfd04a
Sebastian Marines <sebastian0marines@gmail.com>
2021-06-25 14:19:17 -0500

    Add generated emoji url to ecto changeset

--- lib/cutme/links.ex
+++ lib/cutme/links.ex
@@ -50,16 +50,20 @@ defmodule Cutme.Links do
 
   """
   def create_url(attrs \\ %{}) do
+    url = Map.merge(attrs, generate_short_url())
+
     %Url{}
-    |> Url.changeset(attrs)
+    |> Url.changeset(url)
     |> Repo.insert()
   end
 
   def generate_short_url(len \\ 4) do
-    Stream.repeatedly(fn -> Enum.random(Exmoji.all) end)
+    emoji_url = Stream.repeatedly(fn -> Enum.random(Exmoji.all) end)
     |> Enum.take(len)
     |> Enum.map(fn char -> Map.get(char, :unified) end)
     |> Enum.map(fn char -> Exmoji.unified_to_char(char) end)
     |> Enum.reduce("", fn x, acc -> acc <> x end)
+
+    Map.put(%{}, :short_url, emoji_url)
   end
 end
