7ffc88d39f4d9890cf0bfbbbbdfaff44e3f61e0a
Sebastian Marines <sebastian0marines@gmail.com>
2021-06-25 20:09:22 -0500

    ⚡ Add route to redirect from short url to original link

--- lib/cutme_web/controllers/page_controller.ex
+++ lib/cutme_web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule CutmeWeb.PageController do
   use CutmeWeb, :controller
 
+  alias Cutme.Links
+
   def index(conn, _params) do
     render(conn, "index.html")
+  end
+
+  def show(conn, %{"url" => url}) do
+    # TODO handle 404
+    %{link: link} = Links.get_url!(url)
+    redirect(conn, external: link)
   end
 end
--- lib/cutme_web/router.ex
+++ lib/cutme_web/router.ex
@@ -17,6 +17,7 @@ defmodule CutmeWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/:url", PageController, :show
   end
 
   # Other scopes may use custom stacks.
